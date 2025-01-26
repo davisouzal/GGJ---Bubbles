dbb91ea9df2d84082adbb3c10eceac9de54b3e85
Thiago José da Silva Santos <42985926+ThiagoJose007@users.noreply.github.com>
2025-01-26 11:43:09 -0300

    UIS Menus

---
+++ Assets/Scripts/UIs/MenuManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Update is called once per frame
+    public void Jogar()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void AbrirCreditos()
+    {
+
+    }
+    public void FecharCreditos()
+    {
+
+    }
+
+
+    public void Exit()
+    {
+
+    }
+}
