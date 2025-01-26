9a969117a9ffabae00658b8f6ce8232245f2f321
Thiago José da Silva Santos <42985926+ThiagoJose007@users.noreply.github.com>
2025-01-26 15:21:40 -0300

    ajuste

--- Assets/Scripts/UIs/MenuManager.cs
+++ Assets/Scripts/UIs/MenuManager.cs
@@ -5,23 +5,29 @@ public class MenuManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // Update is called once per frame
+    [SerializeField]private string nomedoleveldojogo;
+    [SerializeField]private GameObject painelMenuInicial;
+    [SerializeField]private GameObject painelOpcoes;
     public void Jogar()
     {
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene(nomedoleveldojogo);
     }
 
     public void AbrirCreditos()
     {
-
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
     }
     public void FecharCreditos()
     {
-
+        painelMenuInicial.SetActive(true);
+        painelOpcoes.SetActive(false);
     }
 
 
     public void Exit()
     {
-
+        Application.Quit();
+        Debug.Log("Sai do jogo!");
     }
 }
